42f28ea572ffdbb87afb49fb85f20c867e5bfe8d
ferdyanggara <moeharyoso89@gmail.com>
2021-03-26 16:18:58 +0800

    please make new branch and populate with redu

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,159 +1,206 @@
-import React, {useState, useEffect} from 'react'
-import {StartAction, PauseAction, RestartAction, UpdatingAction} from "../../redux/actions/timeActions"
-import {connect} from 'react-redux'
-import Button from '@material-ui/core/Button';
-import TableUI from '../TableUI';
-import CheckIcon from '@material-ui/icons/Check';
-// import { Pause } from '@material-ui/icons';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-const Timer = (props) => {
-  
-  let startTime = Date.now();
-  const MsToTime = (s) => {
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import GameMap from '../GameMap/GameMap'
 
-  return hrs + ':' + mins + ':' + secs + '.' + ms;
-  }
+import Button from '@material-ui/core/Button'
+import TableUI from '../TableUI'
+import Switch from '@material-ui/core/Switch'
 
-  //setLoop should be seperated from redux
-  const [loop, setLoop] = useState(()=>{});
-   
-  const [counter, setCounter] = useState(1)
-  const [startOnce, setstartOnce] = useState(false)
-  const [intervalFunc, setintervalFunc] = useState({})
-  const [RlapPot1, setRlapPot1] = useState([])
-  const [RlapPot2, setRlapPot2] = useState([])
-  const [RlapPot3, setRlapPot3] = useState([])
-  const [RlapPot4, setRlapPot4] = useState([])
-  const [RlapPot5, setRlapPot5] = useState([])
-  const [BlapPot1, setBlapPot1] = useState([])
-  const [BlapPot2, setBlapPot2] = useState([])
-  const [BlapPot3, setBlapPot3] = useState([])
-  const [BlapPot4, setBlapPot4] = useState([])
-  const [BlapPot5, setBlapPot5] = useState([])
-  const [arrowCounter, setarrowCounter] = useState(0)
+import Grid from '@material-ui/core/Grid'
 
-  const [toggleKeyBinding, settoggleKeyBinding] = useState(false)
-
-  const handleToggleKeyBinding = () => {
-    settoggleKeyBinding(true)
-  }
-
-  const lapAction = () => {
-    console.log("Time elapsed: ", timeElapsed)
-    setarrowCounter(arrowCounter => arrowCounter + 1);
-    setRlapPot1([...RlapPot1,{arrow: arrowCounter , time: timeElapsed}])
-  }
-  
-  useEffect(() => {
-    console.log('lappot1: ', RlapPot1);
-  }, [RlapPot1])
-
-  const makeTime = () => {
-    setstartOnce(true);
-      setintervalFunc( setInterval(() => {
-        settimeElapsed(timeElapsed => timeElapsed - 1);
-        },1000))
-  }
-
- 
-  const StopAction = () => {
-    clearInterval(intervalFunc);
-    console.log("stop")
-  }
-
-
-    
-
-  const [timeElapsed, settimeElapsed] = useState(180)
-
-
-  //redux this 
-  const Restart = () => {
-    clearInterval(intervalFunc);
-    setstartOnce(false)
-    settimeElapsed(180);
-  }
-
-
-  const checkLog = (e) => {
-    // console.log("keycode: ", e.keyCode)
-    if (toggleKeyBinding){
-      if (e.keyCode == 82){
-        console.log('trigger')
-        setTimeout( () => {}, 1000)
-        if (e.keyCode == 49){
-          console.log('bruh')
-          lapAction();
-        } else if (e.keyCode == 50){
-  
-        } else if (e.keyCode == 51){
-          
-        } else if (e.keyCode == 52){
-          
-        } else if (e.keyCode == 53){
-          
-        }
-      } 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    childTable: {
+        flexGrow: 1,
+        paddingLeft: '2vw',
+        marginTop: '5vh',
+        flexDirection: "row"
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    saudara: {
+        flexDirection: 'row',
+        flexWrap: 'nowrap'
     }
-  }
+}))
 
-  document.addEventListener('keypress', checkLog)
 
-  const [selected, setSelected] = React.useState(false);
+const Timer = (props) => {
+    let startTime = Date.now()
+    const MsToTime = (s) => {
+        var ms = s % 1000
+        s = (s - ms) / 1000
+        var secs = s % 60
+        s = (s - secs) / 60
+        var mins = s % 60
+        var hrs = (s - mins) / 60
 
-  const toggleTimeElapsed = () => {
-    timeElapsed == 180 ? settimeElapsed(60) : settimeElapsed(180)
-  }
+        return hrs + ':' + mins + ':' + secs + '.' + ms
+    }
+
+    //setLoop should be seperated from redux
+
+    const [startOnce, setstartOnce] = useState(false)
+    const [intervalFunc, setintervalFunc] = useState({})
+    const [RlapPot1, setRlapPot1] = useState([])
+    const [RlapPot2, setRlapPot2] = useState([])
+    const [RlapPot3, setRlapPot3] = useState([])
+    const [RlapPot4, setRlapPot4] = useState([])
+    const [RlapPot5, setRlapPot5] = useState([])
+    const [BlapPot1, setBlapPot1] = useState([])
+    const [BlapPot2, setBlapPot2] = useState([])
+    const [BlapPot3, setBlapPot3] = useState([])
+    const [BlapPot4, setBlapPot4] = useState([])
+    const [BlapPot5, setBlapPot5] = useState([])
+    const [arrowCounter, setarrowCounter] = useState({ arrow: 1 })
+
+    const [timeElapsed, settimeElapsed] = useState({ time: 180 })
+
+
+
+    const lapAction = () => {
+        setarrowCounter((arrowCounter) => {
+            return {
+                arrow: arrowCounter.arrow + 1
+            }
+        })
+        setRlapPot1([...RlapPot1, { arrowCounter, timeElapsed }])
+    }
+
+    useEffect(() => {
+        // console.log('lappot1: ', RlapPot1)
+    }, [RlapPot1])
+
+    const makeTime = () => {
+        setstartOnce(true)
+        setintervalFunc(
+            setInterval(() => {
+                settimeElapsed((timeElapsed) => {
+                    return {
+                        time: timeElapsed.time - 1
+                    }
+                })
+            }, 1000),
+        )
+    }
+
+    const StopAction = () => {
+        clearInterval(intervalFunc)
+        console.log('stop')
+    }
+
+    //redux this
+    const Restart = () => {
+        clearInterval(intervalFunc)
+        setstartOnce(false)
+        console.log("what is the time: ", timeElapsed)
+        toggleTimer == false ? settimeElapsed((timeElapsed) => {
+            return {
+                time: 180
+            }
+        }) : settimeElapsed((timeElapsed) => {
+            return {
+                time: 60
+            }
+        })
+    }
+
+
+    const [toggleTimer, settoggleTimer] = useState(false)
+
+    const handleToggletimer = (event) => {
+        settoggleTimer(!toggleTimer)
+        timeElapsed.time == 180 ? settimeElapsed((timeElapsed) => {
+            return {
+                time: 60
+            }
+        }) : settimeElapsed((timeElapsed) => {
+            return {
+                time: 180
+            }
+        })
+    }
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>{MsToTime(timeElapsed*1000)}</h1>
+        <>
+            <div>
+                <h1>{MsToTime(timeElapsed.time * 1000)}</h1>
 
-            <Button variant="contained" onClick={toggleTimeElapsed}>Toggle Limit</Button>
-            <Button  variant="contained"disabled={startOnce} onClick={makeTime}>Start</Button>
-            <Button  variant="contained"onClick={Restart}>Restart</Button>
-            <Button  variant="contained"onClick={StopAction}>Stop</Button>
-            <Button  variant="contained"onClick={lapAction}>Lap</Button>
-     
-            <Button  variant="contained"onClick={handleToggleKeyBinding}>KeyBinding</Button>
+                <Switch
+                    checked={toggleTimer}
+                    onChange={handleToggletimer}
+                    color="secondary"
+                    name="toggleTimer"
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+                <Button variant="contained" disabled={startOnce} onClick={makeTime}>
+                    Start
+                </Button>
+                <Button variant="contained" onClick={Restart}>
+                    Restart
+                </Button>
+                <Button variant="contained" onClick={StopAction}>
+                    Stop
+                </Button>
+                <Button variant="contained" onClick={lapAction}>
+                    Lap
+                </Button>
+            </div>
 
-            <TableUI  RlapPot1={RlapPot1}/>
-{/* 
-  <ToggleButton
-    value="check"
-    selected={selected}
-    onChange={() => {
-      setSelected(!selected);
-    }}
-  >
-    <CheckIcon />
-  </ToggleButton> */}
-        </div>
+
+
+            <div className={classes.root}>
+                <Grid container className={classes.saudara}>
+                    <Grid container style={{ maxWidth: "50vw", marginTop: "5vh" }}>
+                        <GameMap />
+                    </Grid>
+                    <Grid container className={classes.childTable}>
+                        {/* 1st row */}
+                        <Grid item xs={6}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        {/* <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid> */}
+                    </Grid>
+                    <Grid container className={classes.childTable}>
+                        {/* 2nd row */}
+                        <Grid item xs={6}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        {/* <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <TableUI RlapPot={RlapPot1} />
+                        </Grid> */}
+                    </Grid>
+                </Grid>
+            </div>
+        </>
     )
 }
 
-const mapStateToProps = (state) => {
-  return({
-    time : state.timer.time,
-    start : state.timer.start,
-    run : state.timer.run,
-  })
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return{
-    startTimer : () => {
-      dispatch(StartAction());},
-    pauseTimer : (currentRun) => {dispatch(PauseAction(currentRun));},
-    updateTimer : (millis) =>{dispatch(UpdatingAction(millis));},
-    restartTimer : () => {dispatch(RestartAction());}
-
-  }
-} 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Timer);
+export default Timer
