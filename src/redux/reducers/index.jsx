86b589558502009ee9a109edcf251c3f37742f81
ferdyanggara <moeharyoso89@gmail.com>
2021-03-28 15:22:05 +0800

    Updates

--- src/redux/reducers/index.jsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import arrowList from "./arrowGroupReducers"
-import alertReducer from './alertReducer';
-
-
-export default combineReducers({ arrowList, alertReducer });
